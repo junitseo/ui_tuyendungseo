f632d37f7fca1d28a288da8b905937c699e263ac
lekhalinh <lekhalinh.oppo@gmail.com>
2023-11-21 16:29:57 +0700

    update login

--- src/views/pages/examples/Login.js
+++ src/views/pages/examples/Login.js
@@ -63,10 +63,11 @@ function Login() {
       password,
     };
     const res = await login(data);
+    console.log(res);
     if (res.success) {
       navigate("/admin/test-post");
     } else {
-      toast.warning("Sai tài khoản hoặc mật khẩu");
+      toast.warning(res.message || "Sai tài khoản hoặc mật khẩu");
     }
   };
   // logic login with google
@@ -224,7 +225,7 @@ function Login() {
                 <a
                   className="text-light"
                   href="/auth/register"
-                  // onClick={(e) => e.preventDefault()}
+                // onClick={(e) => e.preventDefault()}
                 >
                   <small>Tạo tài khoản mới</small>
                 </a>
